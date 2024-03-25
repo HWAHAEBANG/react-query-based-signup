8be1b32db4519689c28525bef5982fe7edbece0a
ChoongLim <bcl0206@naver.com>
2024-03-25 19:57:49 +0900

    feat: 로그인 페이지 구현

--- src/pages/SigninPage.tsx
+++ src/pages/SigninPage.tsx
@@ -1,7 +1,64 @@
+import { useState, FormEvent } from "react"
+import { useNavigate } from "react-router-dom"
+import styles from "./SigninPage.module.scss"
+import { validation } from "utils/validation"
+import InputBox from "components/common/InputBox"
+import RegularButton from "components/common/RegularButton"
+
+interface SigninForm {
+    id: string
+    pw: string
+}
+
 const SigninPage = () => {
-  return (
-    <div>SigninPage</div>
-  )
+    const navigate = useNavigate()
+    const [ form, setForm ] = useState<SigninForm>({id: '', pw: ''})
+
+    const moveToSignin = () => {
+        navigate('/signup')
+    }
+    
+    const handleSubmit = (e:FormEvent<HTMLFormElement> ) => {
+        e.preventDefault()
+        if(!form.id) return alert('아이디를 입력해주세요.')
+        if(!form.pw) return alert('비밀번호를 입력해주세요.')
+
+        alert('로그인 로직 추가 예정')
+    }
+
+    return (
+        <form className={styles.signinForm} onSubmit={handleSubmit}>
+            <section className={styles.inputSection}>
+                <InputBox
+                    inputValue={form}
+                    setInputValue={setForm}
+                    label='아이디' 
+                    name='id' 
+                    placeHolder='ID를 입력해주세요'
+                    rule={validation.id}
+                    />
+                <InputBox
+                    inputValue={form} 
+                    setInputValue={setForm}
+                    label='비밀번호' 
+                    name='pw' 
+                    placeHolder='PW를 입력해주세요'
+                    // rule={validation.pw}
+                    />
+            </section>
+            <section className={styles.buttonSection}>
+                <RegularButton 
+                    onClick={moveToSignin}
+                    type="button">
+                    회원가입
+                </RegularButton>
+                <RegularButton 
+                    type="submit">
+                    로그인
+                </RegularButton>
+            </section>
+        </form>
+    )
 }
 
 export default SigninPage
