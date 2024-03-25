c8daf448fb12b2d66a7d8fb7c3cecfec48759ac2
ChoongLim <bcl0206@naver.com>
2024-03-25 21:42:07 +0900

    feat: query-client 및 provider 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import styles from './App.module.scss';
 import { Outlet } from 'react-router-dom'
 import Header from './components/common/Header';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 function App() {
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 5 * 60 * 1000
+      },
+    },
+  })
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Header/>
       <div className={styles.outletWrapper}>
         <Outlet/>
       </div>
-    </>
+    </QueryClientProvider>
   );
 }
 
