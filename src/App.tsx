da4899a9e530831312335228910e944adfcebfdd
ChoongLim <bcl0206@naver.com>
2024-03-26 17:38:46 +0900

    feat: 세션 컨텍스트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import styles from './App.module.scss';
 import { Outlet } from 'react-router-dom'
 import Header from './components/common/Header';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { SessionProvider } from 'contexts/SessionProvider';
+import useViewportError from 'hooks/useViewportError';
 
 function App() {
-
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
@@ -13,12 +14,16 @@ function App() {
     },
   })
 
+    useViewportError()
+
   return (
     <QueryClientProvider client={queryClient}>
-      <Header/>
-      <div className={styles.outletWrapper}>
-        <Outlet/>
-      </div>
+      <SessionProvider>
+        <Header/>
+        <div className={styles.outletWrapper}>
+          <Outlet/>
+        </div>
+      </SessionProvider>
     </QueryClientProvider>
   );
 }
---
+++ src/contexts/SessionProvider.tsx
@@ -0,0 +1,35 @@
+import useSession from 'hooks/useSession'
+import { useState, createContext, ReactNode } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { UserInfoForm } from 'types/userInfoType'
+
+export const SessionContext = createContext({
+    session: false, 
+    setSession: (value : boolean) => {}, 
+    logout: () =>{}
+})
+
+interface Props {
+    children: ReactNode
+}
+
+export const SessionProvider = ({children}: Props) => {
+    const { checkSession, removeSession } = useSession();
+    const [session, setSession] = useState<any>(checkSession())
+    const navigate = useNavigate()
+
+
+    const logout = () => {
+        if(window.confirm('정말 로그아웃 하시겠습니까?')){
+            removeSession()
+            setSession(false)
+            navigate('/signin')
+        }
+    }
+
+    return(
+        <SessionContext.Provider value={{session, setSession, logout}}>
+            {children}
+        </SessionContext.Provider>
+    )
+}
